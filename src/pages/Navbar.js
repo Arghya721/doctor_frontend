35de8952ac3a10c9501740396cf6b3136079c2d5
Arghya721 <60299552+Arghya721@users.noreply.github.com>
2022-05-04 21:21:28 +0530

    Update Navbar.js

--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -6,7 +6,7 @@ function Navbar() {
     <>
       <nav className="navbar navbar-expand-lg navbar-light bg-light" >
         <div className="container-fluid">
-          <NavLink className="navbar-brand" to="/"><strong>Dr.Buddy</strong></NavLink>
+          <NavLink className="navbar-brand" to="/"><strong>MediCare</strong></NavLink>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
